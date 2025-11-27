5b6b2b565cea1338d6353764b48ef1742c97b1ba
Koki Hasegawa <116789423+KokiHasegawaEspinal@users.noreply.github.com>
2025-11-27 13:47:11 +0900

    Add files via upload
    Elaboracion de un componente muy sencillo

---
+++ Curso_react/trabajo_componentes/src/App.js
@@ -0,0 +1,27 @@
+
+const Micomponente=()=>{
+
+return(
+
+  <div>Hola alumnos</div>
+
+);
+
+
+
+}
+
+const App=()=>{
+
+  return(
+    <div>
+      <div><Micomponente></Micomponente></div>
+      <br/>
+      <div><Micomponente></Micomponente></div>
+    </div>
+  );
+
+
+}
+
+export default App;
